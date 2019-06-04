2d310f5792e252e742279189050c86aca593279c
LukasKodym <lukasz.kodym@gmail.com>
2019-06-04 18:47:32 +0200

    observer pattern introduction and example, end 2.2, 2.3

--- src/main/java/pl/lukas/App.java
+++ src/main/java/pl/lukas/App.java
@@ -1,10 +1,29 @@
 package pl.lukas;
 
+import java.util.Observable;
+import java.util.Observer;
+
 public class App {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
+        Observable observableValue = new Observable() {
+            @Override
+            public void notifyObservers(Object arg) {
+                super.setChanged();
+                super.notifyObservers(arg);
+            }
+        };
 
+        observableValue.addObserver((observable, o) -> System.out.println("1 " + o.toString()));
 
+        observableValue.addObserver(new Observer() {
+            @Override
+            public void update(Observable observable, Object o) {
+                System.out.println("2 " + o.toString());
+            }
+        });
+
+        observableValue.notifyObservers(54);
     }
 }
