5c476b03d88a0185061e84e19a6cba0d30ef6a47
LukasKodym <lukasz.kodym@gmail.com>
2019-06-05 18:15:24 +0200

    strategy pattern introduction - how to use it, end 3.6

--- src/main/java/pl/lukas/App.java
+++ src/main/java/pl/lukas/App.java
@@ -6,6 +6,8 @@ public class App {
 
     public static void main(String[] args) throws InterruptedException {
 
+        // observer pattern
+
         ObservableTempValue observableValue = new ObservableTempValue();
 
         observableValue.addObserver((observable, o) -> System.out.println("1 " + o.toString()));
@@ -16,6 +18,19 @@ public class App {
             observableValue.setValue((int) (observableValue.getOldTemp() + Math.random() * 6 - 2));
         }
 
-        //strategy pattern introduction
+        //strategy pattern
+
+        Employee mike = new Employee();
+
+        mike.travelStrategy = BikeTravelStrategy();
+        mike.jobResponsibility = DoctorJobStrategy();
+        mike.breakfast = SandwichBreakfastStrategy();
+
+        mike.goToWork();
+        mike.doYourJob();
+        mike.eatYourBreakfast();
+
+        mike.travelStrategu = CarTravelStrategy();
+        return;
     }
 }
